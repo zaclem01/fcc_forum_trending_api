8f62e5cad6727ac0035e513d720bbe0535485e43
Zac Clemans <zaclem01@gmail.com>
2016-08-28 20:25:09 -0400

    working prototype

--- routes/routes.js
+++ routes/routes.js
@@ -2,27 +2,14 @@
 
 var express = require('express');
 var router = express.Router();
+var getTrending = require('../utils/getTrending.js');
 var Topic = require('../models/topics.js');
-var getTopicRange = require('../utils/getTopicRange.js');
 
-// router.get('/api', function(req, res) {
-// 	fetchTopics('latest', function(err, topics) {
-// 		if (err) console.log(err);
-// 		res.json(topics);
-// 	});
-// });
-
-router.get('/api/trending/week', function(req, res) {
-	var weekTopics = getTopicRange(7);
-	var allTopics = getTopicRange();
-	Promise.all([weekTopics, allTopics]).then(
-		function(topics) {
-			var week = topics[0];
-			var all = topics[1];
-
-			res.send(week);
-		}
-	);
+router.get('/api/trending/:type', function(req, res) {
+	var type = req.params.type;
+	getTrending(type).then(function(trending) {
+		res.json(trending);
+	});
 });
 
 module.exports = router;
---
+++ utils/getTrending.js
@@ -0,0 +1,134 @@
+"use strict";
+
+var _ = require('underscore');
+
+var Topic = require('../models/topics.js');
+var stopWords = require('./stopWords.js');
+
+
+function getTopicRange(start, end) {
+	return Topic.find({ 
+		post_date: {
+			$lt: new Date( new Date().setDate( new Date().getDate() - end ) ),
+			$gte: new Date( new Date().setDate( new Date().getDate() - start ) )
+		}
+	});
+}
+
+
+function parseTopic(topic) {
+	var title = topic.title;
+
+	var noSpecialChars = title.toLowerCase().replace(/[^a-zA-Z0-9\s]/g, '');
+
+	var split = noSpecialChars.split(/\s+/g);
+	
+	var parsed = [];
+	split.forEach(function(word) {
+		if (stopWords.indexOf(word) === -1 && parsed.indexOf(word) === -1) {
+			parsed.push(word);
+		}
+	});
+
+	return parsed;
+}
+
+
+module.exports = function(type, timePeriods) {
+	return new Promise(function(resolve, reject) {
+		if (timePeriods === undefined) {
+			switch(type) {
+				case 'daily':
+					timePeriods = 7;
+					break;
+				case 'weekly':
+					timePeriods = 4;
+					break;
+				default:
+					timePeriods = 30;
+			}
+		}
+
+		var topics = [];
+		var basis;
+
+		if (type === 'daily') {
+			basis = 1;
+		} else if (type === 'weekly') {
+			basis = 7;
+		} else {
+			return console.error('Trend type not recognized. Try daily or weekly');
+		}
+
+		for (var i = 0; i < timePeriods; i++) {
+			topics.push(getTopicRange((i + 1) * basis, i * basis));
+		}
+
+		var trendTotals = {};
+		var baselineTotals = [];
+
+		Promise.all(topics).then(function(periods) {
+
+			periods.forEach(function(period, index) {
+
+				period.forEach(function(topic) {
+
+					var parsedTopic = parseTopic(topic);
+
+					parsedTopic.forEach(function(word) {
+
+						if (index === 0) {
+							if(!(word in trendTotals)) {
+								trendTotals[word] = 1;
+							} else {
+								trendTotals[word] = trendTotals[word] + 1;
+							}
+						} else {
+							if (!baselineTotals[index]) {
+								baselineTotals[index] = {};
+							}
+							if(!(word in baselineTotals[index])) {
+								baselineTotals[index][word] = 1;
+							} else {
+								baselineTotals[index][word] = baselineTotals[index][word] + 1;
+							}
+						}
+					});
+				});
+			});
+
+			var zScores = [];
+
+			for (var word in trendTotals) {
+				var popSum = 0;
+				var popSize = baselineTotals.length;
+
+				baselineTotals.forEach(function(period) {
+					if (word in period) {
+						popSum += period[word];
+					}
+				});
+
+				var popAvg = popSum / popSize;
+				var popSquares = 0;
+				
+				baselineTotals.forEach(function(period) {
+					if (!(word in period)) {
+						popSquares += Math.pow((0 - popAvg), 2);
+					} else {
+						popSquares += Math.pow((period[word] - popAvg), 2);
+					}
+				});
+
+				var popStd = Math.sqrt(popSquares);
+				var score = popStd == 0 ? 0 : (trendTotals[word] - popAvg) / popStd;
+
+				zScores.push({ word: word, zScore: score });
+			}
+
+			resolve(_.sortBy(zScores, 'zScore').reverse());
+		});
+	});
+
+	
+};
