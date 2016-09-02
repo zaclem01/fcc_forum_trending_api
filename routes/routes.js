bcc4e9edf4dca56e4ab91848cb08214807333cf6
Zac Clemans <zaclem01@gmail.com>
2016-09-02 10:59:27 -0400

    fix route

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var getTrending = require('../utils/getTrending.js');
 var Topic = require('../models/topics.js');
 
-router.get('api/trending', function(req, res) {
+router.get('/api/trending', function(req, res) {
 	res.send('Use api/trending/:type ("daily" or "weekly") to access the trending data');
 });
 
